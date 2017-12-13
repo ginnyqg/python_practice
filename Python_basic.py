710d321102cebe6ac285fb138b901ff487606738
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2017-12-13 14:28:34 -0500

    Update Python_basic.py

--- Python_basic.py
+++ Python_basic.py
@@ -1,24 +1,6 @@
 
-hippos = 0
-answer = 'y'
-while answer == 'y':
-	hippos += 1
-	print(str(hippos) + ' balancing hippos!')
-	answer = input('Add another hippo? (y/n)')
-
-	
-  '''
-1 balancing hippos!
-Add another hippo? (y/n)y
-2 balancing hippos!
-Add another hippo? (y/n)y
-3 balancing hippos!
-Add another hippo? (y/n)y
-4 balancing hippos!
-Add another hippo? (y/n)n
-'''
-
-
+##################################################
+# for loop
 ##################################################
 
 for counter in range(1, 11):
@@ -40,6 +22,32 @@ Emma's Room - Keep out!!!
 
 
 ##################################################
+# while loop
+##################################################
+
+hippos = 0
+answer = 'y'
+while answer == 'y':
+	hippos += 1
+	print(str(hippos) + ' balancing hippos!')
+	answer = input('Add another hippo? (y/n)')
+
+	
+  '''
+1 balancing hippos!
+Add another hippo? (y/n)y
+2 balancing hippos!
+Add another hippo? (y/n)y
+3 balancing hippos!
+Add another hippo? (y/n)y
+4 balancing hippos!
+Add another hippo? (y/n)n
+'''
+
+
+##################################################
+# infinite loop & stop infinte loop
+##################################################
 
 while True:
 	print('This is an infinite loop!')
@@ -48,6 +56,8 @@ while True:
 #Ctrl-C to escape infinite loop
 
 
+##################################################
+# Escape infinite loop
 ##################################################
 
 while True:
@@ -66,6 +76,28 @@ Are you bored yet? (y/n) y
 How rude!
 '''
 
+
+##################################################
+# Nested loop
+##################################################
+
+for hooray_counter in range(1, 4):
+	for hip_counter in range(1, 3):
+		print('Hip')
+	print('Hooray!')
+
+	
+'''	
+Hip
+Hip
+Hooray!
+Hip
+Hip
+Hooray!
+Hip
+Hip
+Hooray!
+'''
 
 
 
