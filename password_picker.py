ed9b3ac546cace679b0c1d406f8a32d48a6a0f1d
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2017-12-13 20:11:30 -0500

    Update password_picker.py

--- password_picker.py
+++ password_picker.py
@@ -12,17 +12,23 @@ nouns = ['apple', 'hammer', 'ball', 'toaster',
 
 print('Welcome to password Picker!')
 
+
+#unlimited number of runs
 while True:
-    
-    adjective = random.choice(adjectives)
-    noun = random.choice(nouns)
-    number = random.randrange(0, 100)
-    special_char = random.choice(string.punctuation)
 
-    password = adjective + noun + str(number) +special_char
+    #for limited number of runs
+    #indentation makes it a subblock of returning 3 passwords at a time
+    for num in range(3):
+        
+        adjective = random.choice(adjectives)
+        noun = random.choice(nouns)
+        number = random.randrange(0, 100)
+        special_char = random.choice(string.punctuation)
 
-    # %s substitute the value after next %
-    print('Your new password is: %s' %password)
+        password = adjective + noun + str(number) +special_char
+
+        # %s substitute the value after next %
+        print('Your new password is: %s' %password)
 
     response = input('Would you like another password? Type y or n: ')
     if response == 'n':
